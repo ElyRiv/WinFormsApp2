5cd79c8094881a412cd096921c6ebdfbb8723ba7
AxelNav <tunagalactic2000@gmail.com>
2023-03-27 14:38:41 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Reflection.Emit
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Por fin lo logre!"
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label2.Text = "Esto está pro"
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) 
+
+    End Sub
+End Class
